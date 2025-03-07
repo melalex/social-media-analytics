f9c992c2e81109a9e28eae6edd34545ac8071ba8
melalex <alexander.melashchenko@gmail.com>
2025-03-07 23:04:56 +0200

    Add lab 2

---
+++ lab2/src/definitions.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+PROJECT_ROOT_DIR = Path(__file__).parent.parent.absolute()
+
+LOGGING_CONFIG_PATH = PROJECT_ROOT_DIR / "logging.ini"
+
+EXTERNAL_DATA_FOLDER = PROJECT_ROOT_DIR / "data" / "external"
+RAW_DATA_FOLDER = PROJECT_ROOT_DIR / "data" / "raw"
+PROCESSED_DATA_FOLDER = PROJECT_ROOT_DIR / "data" / "processed"
